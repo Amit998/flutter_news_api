214d7c3468d93328406bbbe06130b811ebc2ac17
Amit998 <damitlucky998@gmail.com>
2020-08-12 11:32:45 +0530

    News App

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import '../models/item_model.dart';
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+
+final _root = 'https://hacker-news.firebaseio.com/v0';
+
+
+class NewsApiProvider{
+  
+  Client client =Client();
+  
+  fetchIds() async{
+   final response = await client.get('$_root/topstories.json');
+   final ids= json.decode(response.body);
+   return ids;
+
+  }
+  fetchItem(int id) async{
+    final response = await client.get(' $_root/item/$id.json');
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJson(parsedJson);
+  }
+}
