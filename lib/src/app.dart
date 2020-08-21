6b1ee3dc869b8b14c279050f430fd39cd90e3d41
Amit998 <damitlucky998@gmail.com>
2020-08-21 10:24:18 +0530

    Updated

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import './screens/news_list.dart';
+import 'blocs/stories_provider.dart';
 class App extends StatelessWidget{
   Widget build(context){
-    return MaterialApp(
-      title: 'News',
-      home: NewsList(),
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'News',
+        home: NewsList(),
+      ),
     );
   }
 }
